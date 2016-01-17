9b4cb206f8507db4866cd22e050d5ef6df165966
Stanislav Lesnikov <stan@nashdot.com>
2016-01-17 12:24:46 +0100

    Conversion of code blocks

--- src/IssueMigrator/CodePlex/CodePlexParser.cs
+++ src/IssueMigrator/CodePlex/CodePlexParser.cs
@@ -118,6 +118,8 @@ namespace CodePlexIssueMigrator.CodePlex
 		{
 			var text = HttpUtility.HtmlDecode(html);
 			text = text.Replace("\r\n<br>\r\n", "\r\n");
+			text = text.Replace("<pre><code>", "```");
+			text = text.Replace("</code></pre>", "```");
 			return text.Trim();
 		}
 
